44a569c2c3ac3e0eaa990c2dd5085f7acd4acdac
SyuWaG <1525588@chester.ac.uk>
2017-03-19 17:13:19 +0800

    initial Commit

---
+++ SafetyGearsOnline/Site1.Master.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SafetyGearsOnline
+{
+    public partial class Site1 : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void facebook_Click(object sender, ImageClickEventArgs e)
+        {
+            System.Diagnostics.Process.Start("www.facebook.com");
+        }
+
+        protected void twitter_Click(object sender, ImageClickEventArgs e)
+        {
+            System.Diagnostics.Process.Start("www.twitter.com");
+        }
+
+        protected void blog_Click(object sender, ImageClickEventArgs e)
+        {
+            System.Diagnostics.Process.Start("www.blog.com");
+        }
+    }
+}
---
+++ SafetyGearsOnline/pages/Home.aspx
@@ -0,0 +1,8 @@
+﻿<%@ Page Title="Home | Safety & Protective Equipments" Language="C#" MasterPageFile="~/Site1.Master" AutoEventWireup="true" CodeBehind="Home.aspx.cs" Inherits="SafetyGearsOnline.pages.Home" %>
+<asp:Content ID="Content1" ContentPlaceHolderID="head" runat="server">
+</asp:Content>
+<asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder2" runat="server">
+    <asp:Image ID="Image1" runat="server" ImageUrl="~/image/cwlh1.jpg"/>
+</asp:Content>
+<asp:Content ID="Content3" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
+</asp:Content>
